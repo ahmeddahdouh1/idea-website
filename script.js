e136e559838fc99237375e977a7a0fed8e786c56
Ahmed Dahdouh <ahmeddahdouh1234@gamil.com>
2022-09-18 09:30:06 +0300

    init commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let menu = document.querySelector('#menu-btn')
+let navbar =document.querySelector('.navbar')
+
+
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-bars')
+    navbar.classList.toggle('active')
+}
+
+
+
+window.onscroll = () => {
+    menu.classList.remove('fas-bars')
+    navbar.classList.remove('active')
+
+}
