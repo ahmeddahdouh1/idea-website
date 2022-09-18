1453fb832671a51f0e7ca7904cc1cedb94360862
Ahmed Dahdouh <ahmeddahdouh1234@gamil.com>
2022-09-18 17:12:19 +0300

    first editing

--- script.js
+++ script.js
@@ -4,14 +4,30 @@ let navbar =document.querySelector('.navbar')
 
 
 menu.onclick = () =>{
-    menu.classList.toggle('fa-bars')
+    menu.classList.toggle('fa-times')
     navbar.classList.toggle('active')
 }
 
 
 
-window.onscroll = () => {
-    menu.classList.remove('fas-bars')
+window.onscroll = () =>{
+    menu.classList.remove('fas-times')
     navbar.classList.remove('active')
 
 }
+
+
+
+var i=0,text;
+text = 'submit your idea' 
+
+function typing() {
+    if (i<text.length){
+        document.getElementById("text").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typing,50);
+    }
+    
+}
+
+typing();
